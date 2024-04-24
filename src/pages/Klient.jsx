5b502d64cdbcad55f987525798513539ec92c759
Your Name <you@example.com>
2024-04-24 04:28:58 +0300

    commit

--- src/pages/Klient.jsx
+++ src/pages/Klient.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 import AddIcon from "@mui/icons-material/Add";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 
+
+
+import DirectionsCarIcon from '@mui/icons-material/DirectionsCar';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import GroupIcon from "@mui/icons-material/Group";
 import StoreIcon from "@mui/icons-material/Store";
 import SearchIcon from "@mui/icons-material/Search";
 import TableChartIcon from "@mui/icons-material/TableChart";
@@ -88,45 +94,45 @@ const Klient = () => {
             <div className=" flex items-center w-[319px] h-32 ml-5 my-5 bg-white">
               <div className="  pl-5">
                 <h2 className=" pt-4  w-8 h-10 text-[#4094F7] font-bold text-2xl">
-                  24
+                  110,823
                 </h2>
                 <h2 className=" text-[#6E8BB7] text-sm w-[63px] mt-8">
-                  Филиалы
+                  Клиенты
                 </h2>
               </div>
               <span className=" ml-[136px] border tetx-[#E3EFFE] w-[72px] h-[72px] justify-center items-center text-[#4094F7]">
                 <div className=" mt-5 ml-5 w-[28px] h-[28px] ">
-                  <StoreIcon />
+                  <GroupIcon />
                 </div>
               </span>
             </div>
             <div className=" flex items-center w-[319px] h-32 ml-5 my-5 bg-white">
               <div className="  pl-5">
                 <h2 className=" pt-4  w-8 h-10 text-[#4094F7] font-bold text-2xl">
-                  24
+                  110,823
                 </h2>
                 <h2 className=" text-[#6E8BB7] text-sm w-[63px] mt-8">
-                  Филиалы
+                  Подписчики
                 </h2>
               </div>
               <span className=" ml-[136px] border tetx-[#E3EFFE] w-[72px] h-[72px] justify-center items-center text-[#4094F7]">
                 <div className=" mt-5 ml-5 w-[28px] h-[28px] ">
-                  <StoreIcon />
+                  <NotificationsIcon />
                 </div>
               </span>
             </div>
             <div className=" mr-5 flex items-center w-[319px] h-32 ml-5 my-5 bg-white">
               <div className="  pl-5">
                 <h2 className=" pt-4  w-8 h-10 text-[#4094F7] font-bold text-2xl">
-                  24
+                  75
                 </h2>
                 <h2 className=" text-[#6E8BB7] text-sm w-[63px] mt-8">
-                  Филиалы
+                  Курьеры
                 </h2>
               </div>
               <span className=" ml-[136px] border tetx-[#E3EFFE] w-[72px] h-[72px] justify-center items-center text-[#4094F7]">
                 <div className=" mt-5 ml-5 w-[28px] h-[28px] ">
-                  <StoreIcon />
+                  <DirectionsCarIcon />
                 </div>
               </span>
             </div>
@@ -140,10 +146,129 @@ const Klient = () => {
                 <h2 className=" px-[45px] w-[240px] pb-3 font-semibold">
                   Номер телефона
                 </h2>
-                <h2 className=" w-[260px]  px-[100px] ml-10 pb-3 font-semibold">Статус</h2>
+                <h2 className=" w-[260px]  px-[100px] ml-10 pb-3 font-semibold">
+                  Статус
+                </h2>
               </div>
-              <div>
-                
+              <div className="border  items-center h-[48px] flex  px-5">
+                <h2 className=" h-12  pt-3   w-[776px] ">Cody Fisher</h2>
+                <h2 className=" px-[45px] w-[240px] pb-3 ">(907) 555-0101</h2>
+                <span className=" w-[260px]  px-[100px] ">
+                  <span className=" bg-[#4094F726] w-[144px] h-3">
+                    <h2 className=" ml-10 pb-3">Активный</h2>
+                  </span>
+                </span>
+                <span className=" text-[#4094F7]">
+                  <MoreHorizIcon />
+                </span>
+              </div>{" "}
+              <div className="border  items-center h-[48px] flex  px-5">
+                <h2 className=" h-12  pt-3   w-[776px] ">Ronald Richards</h2>
+                <h2 className=" px-[45px] w-[240px] pb-3 ">(319) 555-0115</h2>
+                <span className=" w-[260px]  px-[100px] ">
+                  <span className=" bg-[#4094F726] w-[144px] h-3">
+                    <h2 className=" ml-10 pb-3">Не активный</h2>
+                  </span>
+                </span>
+                <span className=" text-[#4094F7]">
+                  <MoreHorizIcon />
+                </span>
+              </div>
+              <div className="border  items-center h-[48px] flex  px-5">
+                <h2 className=" h-12  pt-3   w-[776px] ">Devon Lane </h2>
+                <h2 className=" px-[45px] w-[240px] pb-3 ">(684) 555-0102</h2>
+                <span className=" w-[260px]  px-[100px] ">
+                  <span className=" bg-[#4094F726] w-[144px] h-3">
+                    <h2 className=" ml-10 pb-3">Активный</h2>
+                  </span>
+                </span>
+                <span className=" text-[#4094F7]">
+                  <MoreHorizIcon />
+                </span>
+              </div>
+              <div className="border  items-center h-[48px] flex  px-5">
+                <h2 className=" h-12  pt-3   w-[776px] ">Kathryn Murphy</h2>
+                <h2 className=" px-[45px] w-[240px] pb-3 ">(252) 555-0126</h2>
+                <span className=" w-[260px]  px-[100px] ">
+                  <span className=" bg-[#4094F726] w-[144px] h-3">
+                    <h2 className=" ml-10 pb-3">Не активный</h2>
+                  </span>
+                </span>
+                <span className=" text-[#4094F7]">
+                  <MoreHorizIcon />
+                </span>
+              </div>{" "}
+              <div className="border  items-center h-[48px] flex  px-5">
+                <h2 className=" h-12  pt-3   w-[776px] ">Darrell Steward</h2>
+                <h2 className=" px-[45px] w-[240px] pb-3 ">(302) 555-0107</h2>
+                <span className=" w-[260px]  px-[100px] ">
+                  <span className=" bg-[#4094F726] w-[144px] h-3">
+                    <h2 className=" ml-10 pb-3">Не активный</h2>
+                  </span>
+                </span>
+                <span className=" text-[#4094F7]">
+                  <MoreHorizIcon />
+                </span>
+              </div>
+              <div className="border  items-center h-[48px] flex  px-5">
+                <h2 className=" h-12  pt-3   w-[776px] ">Dianne Russell </h2>
+                <h2 className=" px-[45px] w-[240px] pb-3 ">(219) 555-0114</h2>
+                <span className=" w-[260px]  px-[100px] ">
+                  <span className=" bg-[#4094F726] w-[144px] h-3">
+                    <h2 className=" ml-10 pb-3">Активный</h2>
+                  </span>
+                </span>
+                <span className=" text-[#4094F7]">
+                  <MoreHorizIcon />
+                </span>
+              </div>{" "}
+              <div className="border  items-center h-[48px] flex  px-5">
+                <h2 className=" h-12  pt-3   w-[776px] ">Jane Cooper</h2>
+                <h2 className=" px-[45px] w-[240px] pb-3 ">(603) 555-0123</h2>
+                <span className=" w-[260px]  px-[100px] ">
+                  <span className=" bg-[#4094F726] w-[144px] h-3">
+                    <h2 className=" ml-10 pb-3">Активный</h2>
+                  </span>
+                </span>
+                <span className=" text-[#4094F7]">
+                  <MoreHorizIcon />
+                </span>
+              </div>
+              <div className="border  items-center h-[48px] flex  px-5">
+                <h2 className=" h-12  pt-3   w-[776px] ">Eleanor Pena</h2>
+                <h2 className=" px-[45px] w-[240px] pb-3 ">(270) 555-0117</h2>
+                <span className=" w-[260px]  px-[100px] ">
+                  <span className=" bg-[#4094F726] w-[144px] h-3">
+                    <h2 className=" ml-10 pb-3">Активный</h2>
+                  </span>
+                </span>
+                <span className=" text-[#4094F7]">
+                  <MoreHorizIcon />
+                </span>
+              </div>
+              <div className="border  items-center h-[48px] flex  px-5">
+                <h2 className=" h-12  pt-3   w-[776px] ">Kristin Watson</h2>
+                <h2 className=" px-[45px] w-[240px] pb-3 ">(205) 555-0100</h2>
+                <span className=" w-[260px]  px-[100px] ">
+                  <span className=" bg-[#4094F726] w-[144px] h-3">
+                    <h2 className=" ml-10 pb-3">Не активный</h2>
+                  </span>
+                </span>
+                <span className=" text-[#4094F7]">
+                  <MoreHorizIcon />
+                </span>
+              </div>
+              <div className="border  items-center h-[48px] flex  px-5">
+                <h2 className=" h-12  pt-3   w-[776px] ">Robert Fox</h2>
+                <h2 className=" px-[45px] w-[240px] pb-3 ">(209) 555-0104</h2>
+                <span className=" w-[260px]  px-[100px] ">
+                  <span className=" bg-[#4094F726] w-[144px] h-3">
+                    <h2 className=" ml-10 pb-3">Не активный</h2>
+                  </span>
+                </span>
+                <span className=" text-[#4094F7]">
+                  <MoreHorizIcon />
+                </span>
               </div>
             </div>
           </div>
